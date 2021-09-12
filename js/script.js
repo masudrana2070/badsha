f6f8d2425598801d7a7286f7380d6a2742866d34
masud rana <freelancermasudrana2070@gmail.com>
2021-09-12 18:37:17 +0600

    test

---
+++ js/script.js
@@ -0,0 +1,60 @@
+setInterval(setClock, 1000)
+
+const hourHand = document.querySelector('[data-hour-hand]')
+const minuteHand = document.querySelector('[data-minute-hand]')
+const secondHand = document.querySelector('[data-second-hand]')
+
+function setClock() {
+    const currentDate = new Date()
+    const secondsRatio = currentDate.getSeconds() / 60
+    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
+    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+
+    setRotation(secondHand, secondsRatio)
+    setRotation(minuteHand, minutesRatio)
+    setRotation(hourHand, hoursRatio)
+
+}
+
+function setRotation(element, rotationRatio) {
+    element.style.setProperty('--rotation', rotationRatio * 360)
+
+}
+
+setClock()
+
+
+
+
+
+
+
+
+function clock () {
+    var today = new Date();
+    
+    var hours = today.getHours();
+    var minutes = today.getMinutes();
+    // var seconds = today.getSeconds();
+
+    document.getElementById('watch').innerHTML = hours+':'+minutes;
+    }
+    setInterval(clock, 1000);
+
+
+
+    function date () {
+        var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+        var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    
+        var date = new Date();
+        
+        var dayName = days[date.getDay()];
+        var day = date.getDate();
+        var monthName = months[date.getMonth()];
+    
+        document.getElementById('date').innerHTML = dayName+', '+monthName+' '+day;
+        
+        }
+        
+        setInterval(date, 1000);
